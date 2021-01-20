cfd26af42835ed611dc612d8609f45e6ccb9bdbe
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2021-01-20 08:31:01 +0100

    First push of Gatsby project

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layouts';
+import Nav from '../components/Nav';
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey poop</p>
+      <p>Hey! I am the home page</p>
+    </>
+  );
+}
